03b8fcc879bb9e8a5df4e4b8e81dd98e1b6b5147
henriquerocharafael <rafaelrocha2500@gmail.com>
2023-01-31 13:53:48 -0300

    added autotags via treesitter

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -1,27 +1,31 @@
 local status, ts = pcall(require, "nvim-treesitter.configs")
-if (not status) then return end
+if not status then
+	return
+end
 
-ts.setup {
-  highlight = {
-    enable = true,
-  },
-  indent = {
-    enable = true,
-  },
-  ensure_installed = {
-    "json",
-    "css",
-    "html",
-    "lua",
-    "javascript"
-  },
-  autotag = {
-    enable = true,
-  },
-  context_commentstring = {
-    enable = true
-  }
-}
-
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.filetype_to_parsername = { "javascript", "javascript.jsx", "typescript.tsx" }
+ts.setup({
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
+	autotag = {
+		enable = true,
+	},
+	context_commentstring = {
+		enable = true,
+	},
+	ensure_installed = {
+		"json",
+		"css",
+		"html",
+		"lua",
+		"javascript",
+		"typescript",
+		"tsx",
+		"bash",
+		"markdown",
+		"yaml",
+	},
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,35 +1,58 @@
 local status, packer = pcall(require, "packer")
-if (not status) then
-  print("Packer is not installed")
-  return
+if not status then
+	print("Packer is not installed")
+	return
 end
 
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
 packer.startup(function(use)
-  use 'wbthomason/packer.nvim' -- Plugin Manager
-  use 'nvim-lualine/lualine.nvim' -- Statusline
-  use 'bluz71/vim-nightfly-colors' -- Colorscheme
-  use 'nvim-lua/plenary.nvim' -- Common utilities
-  use 'onsails/lspkind-nvim' -- vscode-like pictograms
-  use 'hrsh7th/cmp-buffer' -- nvim-cmp source for buffer words
-  use 'hrsh7th/cmp-nvim-lsp' -- nvim-cmp source for neovim's built-in LSP
-  use 'hrsh7th/nvim-cmp' -- Completion
-  use 'neovim/nvim-lspconfig' -- LSP
-  use 'jose-elias-alvarez/null-ls.nvim' -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-  use 'williamboman/mason.nvim' -- Easy way to install a new Language Server
-  use 'williamboman/mason-lspconfig.nvim' -- Dependency of plugin above
-  use "terrortylor/nvim-comment" -- Nvim comments
-  use 'JoosepAlviste/nvim-ts-context-commentstring' -- Comments based in context
+	-- Plugin Manager
+	use("wbthomason/packer.nvim")
 
-  use 'L3MON4D3/LuaSnip' -- snippet engine
-  use {
-    'nvim-treesitter/nvim-treesitter', -- Syntax highlight
-    run = ':TSUpdate'
-  }
-  use 'kyazdani42/nvim-web-devicons' -- File icons
-  use 'nvim-telescope/telescope.nvim' -- Fuzzy finder
-  use 'kyazdani42/nvim-tree.lua' -- Filetree
-  use 'windwp/nvim-autopairs' -- Autoclose pairs
-  use 'lewis6991/gitsigns.nvim' -- Git integration
+	-- Statusline
+	use("nvim-lualine/lualine.nvim")
+
+	-- Colorscheme
+	use("bluz71/vim-nightfly-colors") -- Colorscheme
+
+	-- LSP
+	use("hrsh7th/cmp-buffer") -- nvim-cmp source for buffer words
+	use("hrsh7th/cmp-nvim-lsp") -- nvim-cmp source for neovim's built-in LSP
+	use("hrsh7th/nvim-cmp") -- Completion
+	use("neovim/nvim-lspconfig") -- LSP
+	use("jose-elias-alvarez/null-ls.nvim") -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+	use("williamboman/mason.nvim") -- Easy way to install a new Language Server
+	use("williamboman/mason-lspconfig.nvim") -- Dependency of plugin above
+	use("L3MON4D3/LuaSnip") -- snippet engine
+
+	-- Comments
+	use("terrortylor/nvim-comment") -- Nvim comments
+	use("JoosepAlviste/nvim-ts-context-commentstring") -- Comments based in context
+
+	-- File icons
+	use("kyazdani42/nvim-web-devicons")
+	use("onsails/lspkind-nvim") -- vscode-like pictograms
+
+	-- Telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+
+	-- Filetree
+	use("kyazdani42/nvim-tree.lua")
+
+	-- Treesitter
+	use({
+		"nvim-treesitter/nvim-treesitter", -- Syntax highlight
+		run = ":TSUpdate",
+	})
+	use("windwp/nvim-autopairs") -- Autoclose pairs
+	use("windwp/nvim-ts-autotag") -- Autoclose tags
+
+	-- Git
+	use("lewis6991/gitsigns.nvim") -- Git integration
 end)
