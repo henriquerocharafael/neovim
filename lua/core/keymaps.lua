987d2adaf0d431dd3174b0e38495e02ad463ef02
henriquerocharafael <rafaelrocha2500@gmail.com>
2024-01-28 18:25:09 -0300

    fill changes for remaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,2 +1,14 @@
 -- Leader Key
 vim.g.mapleader = " "
+
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+-- Window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Save file
+keymap("n", "<leader>w", ":w<cr>", opts)
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -8,6 +8,7 @@ opt.expandtab = true
 opt.tabstop = 2
 opt.softtabstop = 2
 opt.shiftwidth = 2
+opt.wrap = false
 
 -- Search Settings
 opt.ignorecase = true
